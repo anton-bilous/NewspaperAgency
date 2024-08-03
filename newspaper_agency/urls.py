c84bb172ee210a5a4fc7fea50ffe3cf730195090
Anton Bilous <anton.bilosus@gmail.com>
2024-08-03 19:56:13 +0300

    Use django-admin-gradient
    Add django-admin-gradient to dependencies and settings

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("newspapers.urls", namespace="newspapers")),
+    path("", include("admin_gradient.urls")),
 ]
