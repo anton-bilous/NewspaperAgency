ad666e1caa70f695add869087cc5f9405c3c6b16
Anton Bilous <anton.bilosus@gmail.com>
2024-08-03 18:45:23 +0300

    Add Topic model

--- newspapers/models.py
+++ newspapers/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField()
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
