2d7618d8f54492cd463249e86a759f87dff377cb
Anton Bilous <anton.bilosus@gmail.com>
2024-08-04 14:10:36 +0300

    Add RedactorDetailView to urls

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, RedactorListView
+from .views import index, RedactorListView, RedactorDetailView
 
 
 app_name = "newspapers"
@@ -8,4 +8,9 @@ app_name = "newspapers"
 urlpatterns = [
     path("", index, name="index"),
     path("redactors/", RedactorListView.as_view(), name="redactors"),
+    path(
+        "redactors/<int:pk>/",
+        RedactorDetailView.as_view(),
+        name="redactor-detail",
+    ),
 ]
