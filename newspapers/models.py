e29b8fe54613fdf08bbc9192e29b787a879297dd
Anton Bilous <anton.bilosus@gmail.com>
2024-08-04 16:33:19 +0300

    Add `__str__` to Topic

--- newspapers/models.py
+++ newspapers/models.py
@@ -9,6 +9,9 @@ class Redactor(AbstractUser):
 class Topic(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Newspaper(models.Model):
     title = models.CharField(max_length=255)
