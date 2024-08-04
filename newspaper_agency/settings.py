3f793752e632dce91d2935894bc79eafa99d2d31
Anton Bilous <anton.bilosus@gmail.com>
2024-08-04 13:33:54 +0300

    Set custom LOGIN_URL in settings

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -134,3 +134,6 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_URL = "/accounts/auth-signin/"
