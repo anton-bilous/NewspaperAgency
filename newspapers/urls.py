b555a569506d084bcd7cf6d04fb02edaa9ea1b62
Anton Bilous <anton.bilosus@gmail.com>
2024-08-04 13:32:40 +0300

    Add url for RedactorListView

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, RedactorListView
 
 
 app_name = "newspapers"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("redactors/", RedactorListView.as_view(), name="redactors"),
 ]
