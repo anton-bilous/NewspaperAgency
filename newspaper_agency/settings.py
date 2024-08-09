836c7917f13086dd050a5b0cf51019a49e043f5a
Anton Bilous <anton.bilosus@gmail.com>
2024-08-09 19:06:02 +0300

    Put admin_gradient on top of other apps

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_gradient.apps.AdminGradientConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "admin_gradient.apps.AdminGradientConfig",
     "crispy_forms",
     "crispy_bootstrap5",
     "newspapers",
