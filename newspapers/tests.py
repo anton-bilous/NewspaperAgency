e99c8f6128a7d12373ca20e50c296aafd6247ea6
Anton Bilous <anton.bilosus@gmail.com>
2024-08-12 17:58:14 +0300

    Add tests for topic model

--- newspapers/tests.py
+++ newspapers/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.db import IntegrityError
 
-# Create your tests here.
+from .models import Topic
+
+
+class TestTopicModel(TestCase):
+    def test_str(self):
+        name = "Test"
+        topic = Topic.objects.create(name=name)
+        self.assertEqual(str(topic), name)
+
+    def test_cant_create_empty_topic(self):
+        with self.assertRaises(IntegrityError):
+            Topic.objects.create()
