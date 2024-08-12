52eea27af9c4c0177122a900b1124cea80a48f45
Anton Bilous <anton.bilosus@gmail.com>
2024-08-12 18:32:06 +0300

    Add anonymous visit counter test

--- newspapers/tests.py
+++ newspapers/tests.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+from django.urls import reverse
 from django.test import TestCase
 from django.db import IntegrityError
 
@@ -33,3 +35,16 @@ class TestNewspaperModel(TestCase):
         topic = Topic.objects.create(name="Test topic")
         with self.assertRaises(IntegrityError):
             Newspaper.objects.create(title="Test", content="", topic=topic)
+
+
+class PublicTestIndexView(TestCase):
+    def test_anonymous_visit_counter(self):
+        url = reverse("newspapers:index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        bs = BeautifulSoup(response.content, "html.parser")
+        self.assertEqual(bs.find(id="anonymous_visit_count").text, "1")
+
+        response = self.client.get(url)
+        bs = BeautifulSoup(response.content, "html.parser")
+        self.assertEqual(bs.find(id="anonymous_visit_count").text, "2")
