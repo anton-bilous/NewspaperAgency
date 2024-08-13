0f637924380f180d456a9236785508f6d9a940c6
Anton Bilous <anton.bilosus@gmail.com>
2024-08-13 20:17:18 +0300

    Update JS to make page selection work

--- static/assets/js/pages/pagination-input.js
+++ static/assets/js/pages/pagination-input.js
@@ -1,20 +1,8 @@
 'use strict';
 
-function adjustWidth(element) {
-    element.style.width = '0px'; // Reset width
-    element.style.width = (element.scrollWidth + 1) + 'px'; // Set width based on content
+function goToPage() {
+    const searchParams = new URLSearchParams(window.location.search);
+    const pageInput = document.getElementById("page-number-input");
+    searchParams.set("page", pageInput.value);
+    window.location.search = searchParams.toString();
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Initialize with default width
-    const page_input = document.getElementById('current-page-input');
-    adjustWidth(page_input);
-
-    page_input.addEventListener("keyup", event => {
-        if (event.key === "Enter") {
-            const searchParams = new URLSearchParams(window.location.search);
-            searchParams.set("page", page_input.value);
-            window.location.search = searchParams.toString();
-        }
-    });
-});
