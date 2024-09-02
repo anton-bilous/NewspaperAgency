4a47e3db0a676fe62e3bb6c1c29c66d7687ce120
Anton Bilous <anton.bilosus@gmail.com>
2024-09-02 18:02:42 +0300

    Configure DB with dj-database-url

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -90,6 +92,13 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+DATABASES["default"].update(
+    dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+)
 
 
 # Password validation
